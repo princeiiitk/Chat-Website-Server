96d67b27725a1e50000e332a8194a68c4f180623
prince kumar <princekumariiitkota@gmail.com>
2024-10-30 00:04:02 +0530

    connection

--- dbConection/connection.js
+++ dbConection/connection.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const mongoDBConnect = async () => {
     try {
         await mongoose.connect(process.env.URL, {
